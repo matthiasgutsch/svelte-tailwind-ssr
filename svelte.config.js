f3ace9ba84a2fbca354d6cb7f58ad6808005b5ce
Matthias Gutsch <matthias.gutsch@gmail.com>
2023-06-27 18:36:07 +0200

    ok

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,23 @@
 // import { resolve } from 'path';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
+
 import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true
-	}),
+    extensions: ['.svelte'],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess({
+        postcss: true
+    }),
 
-	kit: {
-		// adapter: adapter({ out: 'build' }),
-		alias: {
-			$lib: './src/lib/',
-		}
-	}
+    kit: {
+        adapter: adapter({ out: 'build' }),
+        alias: {
+            $lib: './src/lib/',
+        }
+    }
 };
 
 export default config;
