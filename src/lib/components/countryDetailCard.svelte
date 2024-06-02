4a6910a1324b6446d4e56622d6098a492fe8a8cf
Matthias Gutsch <matthias.gutsch@gmail.com>
2024-06-02 21:18:32 +0200

    ok

--- src/lib/components/countryDetailCard.svelte
+++ src/lib/components/countryDetailCard.svelte
@@ -4,45 +4,17 @@
 	export let country: any;
 	export let images: any;
 </script>
-<div
-	class="list-none p-6 mx-auto bg-gray-100 text-gray-800 rounded-md shadow-sm hover:shadow-md flex flex-col items-center max-w-sm"
->
 
-{#each images as images, index (index)}
-		{images.image}
-	{/each}
-
-
-
-	<img class="h-30 w-45" src={country.image} alt={country.page_title} />
-
+<div class="container">
+	<a href="/" transition:fade> Back to List </a>
+	<h1>{country.page_title}</h1>
 	{@html country.description_2}
 
-
-	<div class="py-2 px-4">
-		<span
-			class="inline-block bg-gray-200 rounded-full p-1 mx-1 my-1 text-xs font-semibold text-gray-700"
-			>Alpha2 Code: {country.page_title}</span
-		>
-		<span
-			class="inline-block bg-gray-200 rounded-full p-1 mx-1 my-1 text-xs font-semibold text-gray-700"
-			>Alpha3 Code: {country.page_title}</span
-		>
-		<span
-			class="inline-block bg-gray-200 rounded-full p-1 mx-1 my-1 text-xs font-semibold text-gray-700"
-			>Domain: {country.page_title}</span
-		>
-	</div>
-
-	<!--button to return to country cards grid-->
-	<div class="text-center">
-		<a href="/">
-			<button
-				class="py-1 px-2 text-white bg-blue-900 rounded-lg w-full/2 focus:outline-none"
-				transition:fade
-			>
-				Back to List
-			</button>
-		</a>
+	<div class="row">
+			{#each images as images, index (index)}
+				<img class="img-fluid" src={images.image} alt={country.page_title} />
+				<div class="clearfix"></div>
+				<br/>
+			{/each}
 	</div>
 </div>
--- src/routes/nav.svelte
+++ src/routes/nav.svelte
@@ -1,7 +1,4 @@
-<script>
-	// core components
-	const globeLoc = '../assets/globe.jpg';
-</script>
+
 
 <nav
 	class="flex flex-row content-center justify-between w-full font-sans text-center text-white bg-blue-900 shadow sm:items-baseline"
@@ -10,21 +7,30 @@
 	<div class="self-center">
 		<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/">
 			<div class="flex mx-2">
-				<img class="mx-2 hover:bg-blue-400" src={globeLoc} width="30" height="30" alt="Globe" />
-				<p class="pt-1 hover:bg-blue-400">Countries</p>
+				<p class="pt-1 hover:bg-blue-400">work</p>
+			
+
 			</div>
 		</a>
+
+		<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/about">
+			<div class="flex mx-2">
+				<p class="pt-1 hover:bg-blue-400">about</p>
+			
+
+			</div>
+		</a>
+
+		<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/Contact">
+			<div class="flex mx-2">
+				<p class="pt-1 hover:bg-blue-400">Contact</p>
+			
+
+			</div>
+		</a>
+
+
 	</div>
 
-	<!--navbar RHS-->
-	<div class="self-center">
-		<a class="bg-blue-900 mx-4 rounded hover:bg-blue-400" aria-label="about nav link" href="/about"
-			>About</a
-		>
-		<a
-			class="bg-blue-900 mx-4 rounded hover:bg-blue-400"
-			aria-label="contact nav link"
-			href="/contact">Contact</a
-		>
-	</div>
+	
 </nav>
