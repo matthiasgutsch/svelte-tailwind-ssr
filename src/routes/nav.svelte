51d145b3f65e8508c2546cd9e59ea45ff22d4974
Matthias Gutsch <matthias.gutsch@gmail.com>
2024-06-02 22:01:58 +0200

    ok

--- src/routes/nav.svelte
+++ src/routes/nav.svelte
@@ -5,24 +5,24 @@
 
 <section class="header">
 	<div class="container">
-		<div class="col-4">	<a aria-label="home nav link" transition:fade href="/">
+		<div class="col-4">	<a  href="/" transition:fade>
 			Nexo</a></div>
 	</div>
 </section>
 
-	<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/">
+	<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/" transition:fade>
 		<div class="flex mx-2">
 			<p class="pt-1 hover:bg-blue-400">work</p>
 		</div>
 	</a>
 
-	<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/about">
+	<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/about" transition:fade>
 		<div class="flex mx-2">
 			<p class="pt-1 hover:bg-blue-400">about</p>
 		</div>
 	</a>
 
-	<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/Contact">
+	<a class="mx-2 bg-blue-900" aria-label="home nav link" href="/Contact" transition:fade>
 		<div class="flex mx-2">
 			<p class="pt-1 hover:bg-blue-400">Contact</p>
 		</div>
