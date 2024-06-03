64f172fc93ab4d8b7bbf6ac199c5029c568f6ac5
Matthias Gutsch <matthias.gutsch@gmail.com>
2024-06-03 08:26:57 +0200

    ok

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,12 @@
+<script>
+	import Nav from './nav.svelte';
+</script>
 
+<div class="max-w-6xl mx-auto">
+	<Nav />
+	<slot />
+</div>
 
-	<Nav></Nav>
-	<slot></slot>
-
-
-<slot></slot><style>
-@import url('../styles/global.less');
-</style><slot></slot><script>import { browser } from "$app/environment";
-import { onMount } from "svelte";
-import "../app.scss";
-import Nav from './nav.svelte';
-
-
-onMount(async () => {
-  if (!browser) return;
-
-  // this is enough for most components
-  await import("bootstrap");
-
-  // some components require a bootstrap instance, to fulfil their job. In that case, use this:
-  // const bootstrap = await import("bootstrap");
-  // sample usage: 
-  // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-});</script><style></style>
+<style>
+	@import url('../styles/global.css');
+</style>
