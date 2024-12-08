689ba3532a3a409bf5612b2494b4b6c2dd879b48
Matthias Gutsch <matthias.gutsch@gmail.com>
2024-12-08 17:43:54 +0100

    ok

--- src/lib/components/aboutCard.svelte
+++ src/lib/components/aboutCard.svelte
@@ -9,10 +9,15 @@
 				<h1 class="pb-1 text-xl font-medium leading-8 text-left text-blue-900">About</h1>
 				<div class="col-12 col-md-7">
 					<p class="pb-2">
-						I'm a Senior Web and Graphic Designer specialised in brand identity, creating websites,
-						also for different devices. I have a strong graphic background and web-designer skills.
-						I can work on complete projects from conception to completion independently or as part
-						of a team. I use clean and well structured graphic and webdesign.
+						I am Matthias Gutsch, a Graphic / Web Designer specializing in Frontend Development and
+						UI/UX design. I have a strong traditional graphic design and WordPress, PHP, Angular,
+						CSS Framework Development background and i can work on complete projects from conception
+						to completion independently or as part of a team.
+						<br /><br />
+						I'm a Senior Web and Graphic Designer specialised in brand identity, creating websites, also
+						for different devices. I have a strong graphic background and web-designer skills. I can
+						work on complete projects from conception to completion independently or as part of a team.
+						I use clean and well structured graphic and webdesign.
 						<br /><br />
 						I have gained over ten years experience working in-house, agency side and as a freelancer.
 						I have a background in marketing which allows me to understand brands and make informed design
