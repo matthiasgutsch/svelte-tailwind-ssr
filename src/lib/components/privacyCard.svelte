90e29feb10e0e85ee8b24f032f1da6d81886a5b0
Matthias Gutsch <matthias.gutsch@gmail.com>
2024-12-08 17:38:38 +0100

    ok

---
+++ src/lib/components/privacyCard.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class="container" transition:fade>
+	<div class="profile">
+		<div class="row">
+			<div class="col-12">
+				<h1 class="pb-1 text-xl font-medium leading-8 text-left text-blue-900">Privacy</h1>
+				<div class="col-12 col-md-7">
+					<p class="pb-2" />
+					<br />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+	import PrivacyCard from '$lib/components/privacyCard.svelte';
+</script>
+
+<svelte:head>
+	<title>About Matthias Gutsch</title>
+</svelte:head>
+
+<PrivacyCard />
